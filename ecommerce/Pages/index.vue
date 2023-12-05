b0135f32c43840ef89ec20596951d5d611a982c9
nuriselvia <selviianuri@gmail.com>
2023-12-05 17:50:22 +0700

    tugas-akhir

---
+++ ecommerce/Pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="hero w-full bg-gradient-to-r from-pink-200 via-red-300 to-indigo-400 min-h-screen flex flex-col">
+    <div class="hero-content flex-col lg:flex-row mt-10">
+      <div class="w-[600px] ml-[60px]">
+        <h1 class="text-5xl font-bold text-red-800">Selamat datang di Muslimah Colection</h1>
+        <p class="mt-4 text-lg text-yellow-700">Dibuat dengan material berkualitas tinggi dan desain yang memperhatikan detail, setiap pakaian dari Toko Muslimah Collection akan memenuhi kebutuhan gaya Anda dengan sentuhan kesopanan yang khas. Temukan keindahan dalam kesederhanaan dengan koleksi kami yang elegan dan menawan.</p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ ecommerce/composables/constants/category.ts
@@ -0,0 +1,38 @@
+export const category: Category[] = [
+    {
+    id: 1,
+    name: "Pashmina",
+    },
+    {
+    id: 2,
+    name: "Sport",
+    },
+    {
+    id: 3,
+    name: "Segi Empat",
+    },
+    {
+    id: 4,
+    name: "Bergo",
+    },
+    {
+    id: 5,
+    name: "Ciput",
+    },
+    {
+    id: 6,
+    name: "Cadar",
+    },
+    {
+    id: 7,
+    name: "Dress",
+    },
+    {
+    id: 8,
+    name: "Kaftan",
+    },
+    ]
+    export interface Category {
+    id? : number,
+    name?: string,
+    }
